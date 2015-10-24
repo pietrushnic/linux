f52971f19a57ce2a6cf2d752330cbc40a87fc376
Piotr Król <piotr.krol@3mdeb.com>
2015-10-25 00:25:42 +0200

    apply backport SmPL patch collateral-evolutions/network/0036-ethtool_eee/ethtool_eee.cocci

--- backports/drivers/net/ethernet/intel/igb/igb_ethtool.c
+++ backports/drivers/net/ethernet/intel/igb/igb_ethtool.c
@@ -2610,6 +2610,7 @@ static int igb_set_rxnfc(struct net_device *dev, struct ethtool_rxnfc *cmd)
 	return ret;
 }
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,6,0)
 static int igb_get_eee(struct net_device *netdev, struct ethtool_eee *edata)
 {
 	struct igb_adapter *adapter = netdev_priv(netdev);
@@ -2687,7 +2688,9 @@ static int igb_get_eee(struct net_device *netdev, struct ethtool_eee *edata)
 
 	return 0;
 }
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(3,6,0) */
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,6,0)
 static int igb_set_eee(struct net_device *netdev,
 		       struct ethtool_eee *edata)
 {
@@ -2761,6 +2764,7 @@ static int igb_set_eee(struct net_device *netdev,
 
 	return 0;
 }
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(3,6,0) */
 
 static int igb_get_module_info(struct net_device *netdev,
 			       struct ethtool_modinfo *modinfo)
@@ -3090,8 +3094,12 @@ static const struct ethtool_ops igb_ethtool_ops = {
 #endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(3,5,0) */
 	.get_rxnfc		= igb_get_rxnfc,
 	.set_rxnfc		= igb_set_rxnfc,
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,6,0)
 	.get_eee		= igb_get_eee,
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(3,6,0) */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,6,0)
 	.set_eee		= igb_set_eee,
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(3,6,0) */
 	.get_module_info	= igb_get_module_info,
 	.get_module_eeprom	= igb_get_module_eeprom,
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3,16,0)
